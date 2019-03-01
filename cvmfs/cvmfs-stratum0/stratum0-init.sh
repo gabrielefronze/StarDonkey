6ae768682a4632a6d37be73a6b9a9615486efff1
Gabriele <sucre.91@hotmail.it>
2019-03-01 15:38:27 +0100

    Adding comments in init scripts

--- cvmfs/cvmfs-stratum0/stratum0-init.sh
+++ cvmfs/cvmfs-stratum0/stratum0-init.sh
@@ -1,3 +1,8 @@
+# Creating repository
 cvmfs_server mkfs -o root "$CVMFS_REPO_NAME"
+
+# Putting repository in editing state
 cvmfs_server transaction "$CVMFS_REPO_NAME"
+
+# Pushing empty changes, for good measure (something like "first commit")
 cvmfs_server publish "$CVMFS_REPO_NAME"
