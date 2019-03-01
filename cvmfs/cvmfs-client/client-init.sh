bad52e54e5285b0b50f1ea0bd0dc4cbae826b9fd
Gabriele <sucre.91@hotmail.it>
2019-03-01 15:32:59 +0100

    Moving client config of docker file to init-script

--- cvmfs/cvmfs-client/client-init.sh
+++ cvmfs/cvmfs-client/client-init.sh
@@ -1,3 +1,9 @@
+echo "CVMFS_SERVER_URL=$CVMFS_STRATUM1_URL/cvmfs/$CVMFS_REPO_NAME" > /etc/cvmfs/default.local
+echo "CVMFS_REPOSITORIES=$CVMFS_REPO_NAME" >> /etc/cvmfs/default.local
+echo "CVMFS_HTTP_PROXY=DIRECT" >> /etc/cvmfs/default.local
+echo "CVMFS_CACHE_BASE=/cvmfs-cache" >> /etc/cvmfs/default.local
+echo "CVMFS_QUOTA_LIMIT=10240" >> /etc/cvmfs/default.local
+
 setenforce 0
 cvmfs_config setup
 service autofs restart
