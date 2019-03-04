8219d282a460cda49e494e0327f68711b7613f40
Gabriele <sucre.91@hotmail.it>
2019-03-04 18:14:54 +0100

    No need to mkdir for keys

--- cvmfs/propagate-keys.sh
+++ cvmfs/propagate-keys.sh
@@ -1,5 +1,2 @@
-docker exec --user root -ti cvmfs-stratum1 mkdir /etc/cvmfs/keys
 docker cp /var/cvmfs-docker/stratum0/etc/cvmfs/keys/virgo.sw.pub cvmfs-stratum1:/etc/cvmfs/keys
-
-docker exec --user root -ti cvmfs-client mkdir /etc/cvmfs/keys
 docker cp /var/cvmfs-docker/stratum0/etc/cvmfs/keys/virgo.sw.pub cvmfs-client:/etc/cvmfs/keys
