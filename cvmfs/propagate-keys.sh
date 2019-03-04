992b9dd00dcc69bac1df6bc7f8e662ea5351e351
Gabriele <sucre.91@hotmail.it>
2019-03-04 18:13:44 +0100

    Copying between containers is not supported

--- cvmfs/propagate-keys.sh
+++ cvmfs/propagate-keys.sh
@@ -1,5 +1,5 @@
 docker exec --user root -ti cvmfs-stratum1 mkdir /etc/cvmfs/keys
-docker cp cvmfs-stratum0:/etc/cvmfs/keys/virgo.sw.pub cvmfs-stratum1:/etc/cvmfs/keys
+docker cp /var/cvmfs-docker/stratum0/etc/cvmfs/keys/virgo.sw.pub cvmfs-stratum1:/etc/cvmfs/keys
 
 docker exec --user root -ti cvmfs-client mkdir /etc/cvmfs/keys
-docker cp cvmfs-stratum0:/etc/cvmfs/keys/virgo.sw.pub cvmfs-client:/etc/cvmfs/keys
+docker cp /var/cvmfs-docker/stratum0/etc/cvmfs/keys/virgo.sw.pub cvmfs-client:/etc/cvmfs/keys
