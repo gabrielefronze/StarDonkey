08c1e8e3d8521ae9364348d25620818854524e8b
Gabriele <sucre.91@hotmail.it>
2019-03-15 18:31:38 +0100

    Asking for the repository name if not defined

--- cvmfs/clear-var-cvmfs.sh
+++ cvmfs/clear-var-cvmfs.sh
@@ -1,6 +1,6 @@
 echo "WARNING: this script is intended to remove ALL CVMFS data in a test environment."
 echo "If you are not COMPLETELY SURE of what will happen EXIT IMMEDIATELY using Ctrl-C!"
-read -p "Otherwise press ENTER key to continue..."
+read -p "Otherwise press ENTER to continue..."
 
 # Unmounting folders
 umount overlay_virgo.sw
--- cvmfs/cvmfs_server_aliases.sh
+++ cvmfs/cvmfs_server_aliases.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-sh "${BASH_SOURCE[0]}"/cvmfs-variable.env
-
 function cvmfs_server {
+    if [[ -z "$CVMFS_REPO_NAME" ]]
+    then
+        read -p "Enter the repository name: " reponame
+        export CVMFS_REPO_NAME=$reponame
+    else
+        echo "The operations will be performed on the repository $CVMFS_REPO_NAME"
+        read -p "Press ENTER key to continue, Ctrl-C to abort..."
+    fi
+
     docker exec -ti cvmfs_server cvmfs-stratum0 $@
 
     if [[ "$1" == "transaction" ]]
