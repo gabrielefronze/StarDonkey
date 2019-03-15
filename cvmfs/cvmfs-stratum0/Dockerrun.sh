928889155fb60efa259ee93f2d76601d62fe51d5
Gabriele <sucre.91@hotmail.it>
2019-03-15 18:51:18 +0100

    Using bind mount for /srv and /etc as well

--- cvmfs/cvmfs-stratum0/Dockerrun.sh
+++ cvmfs/cvmfs-stratum0/Dockerrun.sh
@@ -11,7 +11,7 @@ docker run -d \
 --env-file ../cvmfs-variables.env \
 --mount type=bind,source=/var/cvmfs-docker/stratum0/var-spool-cvmfs,target=/var/spool/cvmfs,bind-propagation=rshared,consistency=consistent \
 --mount type=bind,source=/var/cvmfs-docker/stratum0/cvmfs,target=/cvmfs,bind-propagation=rshared,consistency=consistent \
---volume /var/cvmfs-docker/stratum0/srv-cvmfs:/srv/cvmfs \
---volume /var/cvmfs-docker/stratum0/etc-cvmfs:/etc/cvmfs \
+--mount type=bind,source=/var/cvmfs-docker/stratum0/srv-cvmfs,target=/srv/cvmfs,bind-propagation=rshared,consistency=consistent \
+--mount type=bind,source=/var/cvmfs-docker/stratum0/etc-cvmfs,target=/etc/cvmfs,bind-propagation=rshared,consistency=consistent \
 --volume /sys/fs/cgroup:/sys/fs/cgroup \
 slidspitfire/cvmfs-stratum0-latest:latest
