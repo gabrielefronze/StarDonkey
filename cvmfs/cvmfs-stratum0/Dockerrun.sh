73a8505f39fecb6ede8b735a399941388a836d35
Gabriele <sucre.91@hotmail.it>
2019-03-15 12:19:20 +0100

    Simplyfying directory tree and creating mount bind endpoints

--- cvmfs/cvmfs-stratum0/Dockerrun.sh
+++ cvmfs/cvmfs-stratum0/Dockerrun.sh
@@ -1,14 +1,18 @@
 docker build -t slidspitfire/cvmfs-stratum0-latest:latest .
 
+mkdir /var/cvmfs-docker/stratum0
+mkdir /var/cvmfs-docker/stratum0/var-spool-cvmfs
+mkdir /var/cvmfs-docker/stratum0/cvmfs
+
 docker run -d \
 -p 80:80 -p 8000:8000 \
 --name cvmfs-stratum0 \
 --hostname cvmfs-stratum0 \
 --privileged \
 --env-file ../cvmfs-variables.env \
---mount type=bind,source=/var/cvmfs-docker/stratum0/var/spool/cvmfs,target=/var/spool/cvmfs \
+--mount type=bind,source=/var/cvmfs-docker/stratum0/var-spool-cvmfs,target=/var/spool/cvmfs \
 --mount type=bind,source=/var/cvmfs-docker/stratum0/cvmfs,target=/cvmfs \
---volume /var/cvmfs-docker/stratum0/srv/cvmfs:/srv/cvmfs \
---volume /var/cvmfs-docker/stratum0/etc/cvmfs:/etc/cvmfs \
+--volume /var/cvmfs-docker/stratum0/srv-cvmfs:/srv/cvmfs \
+--volume /var/cvmfs-docker/stratum0/etc-cvmfs:/etc/cvmfs \
 --volume /sys/fs/cgroup:/sys/fs/cgroup \
 slidspitfire/cvmfs-stratum0-latest:latest
--- cvmfs/cvmfs-stratum1/Dockerrun.sh
+++ cvmfs/cvmfs-stratum1/Dockerrun.sh
@@ -1,14 +1,18 @@
 docker build -t slidspitfire/cvmfs-stratum1-latest:latest .
 
+mkdir /var/cvmfs-docker/stratum1
+mkdir /var/cvmfs-docker/stratum1/var-spool-cvmfs
+mkdir /var/cvmfs-docker/stratum1/cvmfs
+
 docker run -d \
 -p 80:80 -p 8000:8000 \
 --name cvmfs-stratum1 \
 --hostname cvmfs-stratum1 \
 --privileged \
 --env-file ../cvmfs-variables.env \
---mount type=bind,source=/var/cvmfs-docker/stratum1/var/spool/cvmfs,target=/var/spool/cvmfs \
+--mount type=bind,source=/var/cvmfs-docker/stratum1/var-spool-cvmfs,target=/var/spool/cvmfs \
 --mount type=bind,source=/var/cvmfs-docker/stratum1/cvmfs,target=/cvmfs \
---volume /var/cvmfs-docker/stratum1/srv/cvmfs:/srv/cvmfs \
---volume /var/cvmfs-docker/stratum1/etc/cvmfs:/etc/cvmfs \
+--volume /var/cvmfs-docker/stratum1/srv-cvmfs:/srv/cvmfs \
+--volume /var/cvmfs-docker/stratum1/etc-cvmfs:/etc/cvmfs \
 --volume /sys/fs/cgroup:/sys/fs/cgroup \
 slidspitfire/cvmfs-stratum1-latest:latest
