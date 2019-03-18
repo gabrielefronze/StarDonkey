72f1e5ee089342ac93e7ff0d84263abe30516b89
Gabriele <sucre.91@hotmail.it>
2019-03-18 17:43:52 +0100

    Making /etc/httpd/conf.d permanent

--- cvmfs/cvmfs-stratum0/Dockerrun.sh
+++ cvmfs/cvmfs-stratum0/Dockerrun.sh
@@ -4,6 +4,7 @@ mkdir -p /var/cvmfs-docker/stratum0/var-spool-cvmfs
 mkdir -p /var/cvmfs-docker/stratum0/cvmfs
 mkdir -p /var/cvmfs-docker/stratum0/srv-cvmfs
 mkdir -p /var/cvmfs-docker/stratum0/etc-cvmfs
+mkdir -p /var/cvmfs-docker/stratum0/etc-httpd-confd
 
 docker run -d \
 -p 80:80 -p 8000:8000 \
@@ -16,5 +17,6 @@ docker run -d \
 --mount type=bind,source=/var/cvmfs-docker/stratum0/cvmfs,target=/cvmfs,bind-propagation=rshared,consistency=consistent \
 --mount type=bind,source=/var/cvmfs-docker/stratum0/srv-cvmfs,target=/srv/cvmfs,bind-propagation=rshared,consistency=consistent \
 --mount type=bind,source=/var/cvmfs-docker/stratum0/etc-cvmfs,target=/etc/cvmfs,bind-propagation=rshared,consistency=consistent \
+--mount type=bind,source=/var/cvmfs-docker/stratum0/etc-httpd-confd,target=/etc/httpd/conf.d,bind-propagation=rshared,consistency=consistent \
 --volume /sys/fs/cgroup:/sys/fs/cgroup \
 slidspitfire/cvmfs-stratum0-latest:latest
