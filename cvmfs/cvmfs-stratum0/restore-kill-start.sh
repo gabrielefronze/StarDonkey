78054678699d55bd29e74ae53be37c6f27d00d29
Gabriele <sucre.91@hotmail.it>
2019-03-19 10:15:48 +0100

    Using env variable in restore-kill-start script

--- cvmfs/cvmfs-stratum0/restore-kill-start.sh
+++ cvmfs/cvmfs-stratum0/restore-kill-start.sh
@@ -1,6 +1,6 @@
 # unmount dangling mount points
-umount /cvmfs/virgo.sw
-umount /var/spool/cvmfs/virgo.sw/rdonly
+umount /cvmfs/$CVMFS_REPO_NAME
+umount /var/spool/cvmfs/$CVMFS_REPO_NAME/rdonly
 
 # Let cvmfs remount them for you
-cvmfs_server mount virgo.sw
+cvmfs_server mount $CVMFS_REPO_NAME
