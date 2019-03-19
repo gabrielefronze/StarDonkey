06eac836048c54594009b4f74db4135719857b06
Gabriele <sucre.91@hotmail.it>
2019-03-19 10:16:08 +0100

    Adding restore-prune-start script

---
+++ cvmfs/cvmfs-stratum0/restore-prune-start.sh
@@ -0,0 +1,9 @@
+# Mount rdonly and overlay directory using fstab contents
+mount cvmfs2#$CVMFS_REPO_NAME
+mount overlay_$CVMFS_REPO_NAME
+
+# Eventually remove transaction locks left dangling: the above mounts happen to be read-only
+rm -f /var/spool/cvmfs/$CVMFS_REPO_NAME/in_transaction.lock
+
+# Remount everything using cvmfs_server
+cvmfs_server mount $CVMFS_REPO_NAME
