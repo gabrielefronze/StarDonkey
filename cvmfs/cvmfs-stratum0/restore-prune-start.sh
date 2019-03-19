f0871d92cce6eae68ce029d89d7a2824bf086697
Gabriele <sucre.91@hotmail.it>
2019-03-19 12:45:17 +0100

    Adding left arounds removal to restore-prune-start script

--- cvmfs/cvmfs-stratum0/restore-prune-start.sh
+++ cvmfs/cvmfs-stratum0/restore-prune-start.sh
@@ -1,12 +1,21 @@
+# Unmount everything mounted by fstab
+echo "Unmounting $CVMFS_REPO_NAME left arounds..."
+umount overlay_virgo.sw
+umount /dev/fuse
+
 # Mount rdonly and overlay directory using fstab contents
+echo "Mouting $CVMFS_REPO_NAME fstab entires..."
 mount cvmfs2#$CVMFS_REPO_NAME
 mount overlay_$CVMFS_REPO_NAME
 
 # Restore unit mounts
+echo "Restoring systemd mount services..."
 cp /cvmfs-backup/* /run/systemd/generator
 
 # Eventually remove transaction locks left dangling: the above mounts happen to be read-only
+echo "Removing transaction locks if any..."
 rm -f /var/spool/cvmfs/$CVMFS_REPO_NAME/in_transaction.lock
 
 # Remount everything using cvmfs_server
+echo "Mounting cvmfs $CVMFS_REPO_NAME repository..."
 cvmfs_server mount $CVMFS_REPO_NAME
