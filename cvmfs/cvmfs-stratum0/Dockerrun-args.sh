8a0d2e2800bfb905ae1ef0fff0fd5a0e5bb255b6
Gabriele <sucre.91@hotmail.it>
2019-03-20 17:19:46 +0100

    Avoiding fstab and httpd.conf bind mount

--- cvmfs/cvmfs-stratum0/Dockerrun-args.sh
+++ cvmfs/cvmfs-stratum0/Dockerrun-args.sh
@@ -6,8 +6,6 @@ mkdir -p "$CVMFS_ROOT_DIR"/var-spool-cvmfs
 mkdir -p "$CVMFS_ROOT_DIR"/cvmfs
 mkdir -p "$CVMFS_ROOT_DIR"/srv-cvmfs
 mkdir -p "$CVMFS_ROOT_DIR"/etc-cvmfs
-mkdir -p "$CVMFS_ROOT_DIR"/etc-httpd-confd
-touch "$CVMFS_ROOT_DIR"/etc-fstab
 
 docker run -d \
 -p 80:80 -p 8000:8000 \
@@ -19,8 +17,6 @@ docker run -d \
 --mount type=bind,source="$CVMFS_ROOT_DIR"/cvmfs,target=/cvmfs,bind-propagation=rshared,consistency=consistent \
 --mount type=bind,source="$CVMFS_ROOT_DIR"/srv-cvmfs,target=/srv/cvmfs,bind-propagation=rshared,consistency=consistent \
 --mount type=bind,source="$CVMFS_ROOT_DIR"/etc-cvmfs,target=/etc/cvmfs,bind-propagation=rshared,consistency=consistent \
---mount type=bind,source="$CVMFS_ROOT_DIR"/etc-httpd-confd,target=/etc/httpd/conf.d,bind-propagation=rshared,consistency=consistent \
---mount type=bind,source="$CVMFS_ROOT_DIR"/etc-fstab,target=/etc/fstab,bind-propagation=rshared,consistency=consistent \
 --volume /sys/fs/cgroup:/sys/fs/cgroup \
 "$CVMFS_CONTAINER_IMAGE_NAME"
 
