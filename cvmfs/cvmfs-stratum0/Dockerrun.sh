2cd283e32fb057570e5458bf77152e1d2289a476
Gabriele <sucre.91@hotmail.it>
2019-03-20 17:11:49 +0100

    Fixing base image name

--- cvmfs/cvmfs-stratum0/Dockerrun.sh
+++ cvmfs/cvmfs-stratum0/Dockerrun.sh
@@ -1,5 +1,5 @@
 CVMFS_ROOT_DIR=/var/cvmfs-docker/stratum0
-CVMFS_CONTAINER_IMAGE_NAME=slidspitfire/cvmfs-stratum0-latest:latest
+CVMFS_CONTAINER_IMAGE_NAME=slidspitfire/cvmfs-stratum0-base:latest
 ENV_FILE=../cvmfs-variables.env
 
 sh Dockerrun-args.sh $CVMFS_ROOT_DIR $CVMFS_CONTAINER_IMAGE_NAME $ENV_FILE
