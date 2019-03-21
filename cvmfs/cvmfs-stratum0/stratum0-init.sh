a318ffbfb4a169d842f97668f717a4d60305ad9f
Gabriele <sucre.91@hotmail.it>
2019-03-21 11:04:41 +0100

    Using check instead of empty transaction-publish to verify new repo

--- cvmfs/cvmfs-stratum0/stratum0-init.sh
+++ cvmfs/cvmfs-stratum0/stratum0-init.sh
@@ -4,7 +4,4 @@ CVMFS_REPO_NAME_PRIVATE=${1:-CVMFS_REPO_NAME}
 cvmfs_server mkfs -o root "$CVMFS_REPO_NAME_PRIVATE"
 
 # Putting repository in editing state
-cvmfs_server transaction "$CVMFS_REPO_NAME_PRIVATE"
-
-# Pushing empty changes, for good measure (something like "first commit")
-cvmfs_server publish "$CVMFS_REPO_NAME_PRIVATE"
+cvmfs_server check "$CVMFS_REPO_NAME_PRIVATE"
