7dae225c757e2f3801d946c6b5214c4fcdff0720
Gabriele <sucre.91@hotmail.it>
2019-03-21 11:06:02 +0100

    Removal of old restore scripts since replaced by restore-repo.sh

--- cvmfs/cvmfs-stratum0/restore-kill-start.sh
+++
@@ -1,8 +0,0 @@
-CVMFS_REPO_NAME_PRIVATE=${1:-CVMFS_REPO_NAME}
-
-# unmount dangling mount points
-umount /cvmfs/$CVMFS_REPO_NAME_PRIVATE
-umount /var/spool/cvmfs/$CVMFS_REPO_NAME_PRIVATE/rdonly
-
-# Let cvmfs remount them for you
-cvmfs_server mount $CVMFS_REPO_NAME_PRIVATE
