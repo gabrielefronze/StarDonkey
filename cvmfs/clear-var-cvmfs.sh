a4744e312e4d1ceb3b659a60437a0c6fefa8b65b
Gabriele <sucre.91@hotmail.it>
2019-03-22 13:21:57 +0100

    Expand clearing script

--- cvmfs/clear-var-cvmfs.sh
+++ cvmfs/clear-var-cvmfs.sh
@@ -1,10 +1,21 @@
 echo "WARNING: this script is intended to remove ALL CVMFS data in a test environment."
 echo "If you are not COMPLETELY SURE of what will happen EXIT IMMEDIATELY using Ctrl-C!"
-read -p "Otherwise press ENTER to continue..."
+read -p "Otherwise press [ENTER] to continue..."
+
+if [[ -z $1 ]]; then
+    read -p "Please provide the CVMFS host root and pres [ENTER]: " HOST_CVMFS_ROOT_DIR
+else
+    HOST_CVMFS_ROOT_DIR="$1"
+fi
 
 # Unmounting folders
-umount overlay_virgo.sw
+REPO_NAME_ARRAY=$(ls $HOST_CVMFS_ROOT_DIR/srv-cvmfs/ | tr " " "\n" | sed "/info/d")
+
+for REPO_NAME in $REPO_NAME_ARRAY
+do
+    umount overlay_"$REPO_NAME"
+done
 umount /dev/fuse
 
 # Removing the tree folder
-rm -rf /var/cvmfs-docker/
+rm -rf $HOST_CVMFS_ROOT_DIR
