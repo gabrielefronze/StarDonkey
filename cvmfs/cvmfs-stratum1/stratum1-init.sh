7eb662bb8475f979625e885bf69f2df5d32dde5a
Gabriele <sucre.91@hotmail.it>
2019-03-22 13:05:03 +0100

    Removing init scripts from stratums

--- cvmfs/cvmfs-stratum1/stratum1-init.sh
+++
@@ -1,5 +0,0 @@
-# Setting up stratum1 to replicate stratum0 repo
-cvmfs_server add-replica -o root "$CVMFS_STRATUM0_URL:$CVMFS_STRATUM0_PORT/cvmfs/$CVMFS_REPO_NAME" "$CVMFS_REPO_KEY"
-
-# Getting sychronized with stratum0
-cvmfs_server snapshot
