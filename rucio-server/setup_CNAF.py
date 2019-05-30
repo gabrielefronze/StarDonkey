b4587656142271cac3478a9abefc97e9ff90a0e6
gabrielefronze <sucre.91@hotmail.it>
2019-05-30 18:12:40 +0200

    Update of RSEs params

--- rucio-server/setup_CNAF.py
+++ rucio-server/setup_CNAF.py
@@ -24,35 +24,46 @@ if __name__ == '__main__':
 
     add_account('jdoe', 'USER', 'test', 'root')
 
-    params = {'scheme': 'file',
-              'prefix': '/tmp/SITE1_DISK/',
-              'impl': 'rucio.rse.protocols.srm.Default',
+    # gsiftp://gridftp-plain-virgo.cr.cnaf.infn.it:2811/storage/gpfs_virgo4/Runs/rucio/
+    params = {'scheme': 'gsiftp',
+              'prefix': '/storage/gpfs_virgo4/Runs/rucioTo',
+              'hostname': 'gridftp-plain-virgo.cr.cnaf.infn.it',
+              'port': 2811,
+              'impl': 'rucio.rse.protocols.gsiftp.Default',
               'domains': {"lan": {"read": 1,
                                   "write": 1,
-                                  "delete": 1},
+                                  "delete": 1,
+                                  "third_party_copy": 1},
                           "wan": {"read": 1,
                                   "write": 1,
-                                  "delete": 1}}}
+                                  "delete": 1,
+                                  "third_party_copy": 1}}}
 
     add_rse('CNAF_STORM', 'root')
     add_protocol('CNAF_STORM', params)
     add_rse_attribute(rse='CNAF_STORM', key='istape', value='False')
-    add_rse_attribute(rse='CNAF_STORM', key='supported_checksums', value='adler32')
+    add_rse_attribute(rse='CNAF_STORM', key='supported_checksums', value='md5')
 
-    params = {'scheme': 'file',
-              'prefix': '/tmp/SITE2_DISK/',
+    # srm://storm-fe-archive.cr.cnaf.infn.it:8444/srm/managerv2?SFN=/virgoplain/
+    params = {'scheme': 'srm',
+              'prefix': '/virgoplain/rucioTo',
+              'hostname': 'storm-fe-archive.cr.cnaf.infn.it',
+              'port': 8444,
+              'web-service-path': '/srm/managerv2?SFN=',
               'impl': 'rucio.rse.protocols.gfal.Default',
               'domains': {"lan": {"read": 1,
                                   "write": 1,
-                                  "delete": 1},
+                                  "delete": 1,
+                                  "third_party_copy": 1},
                           "wan": {"read": 1,
                                   "write": 1,
-                                  "delete": 1}}}
+                                  "delete": 1,
+                                  "third_party_copy": 1}}}
 
-    add_rse('CNAF_GFTP', 'root')
-    add_protocol('CNAF_GFTP', params)
-    add_rse_attribute(rse='CNAF_GFTP', key='istape', value='False')
-    add_rse_attribute(rse='CNAF_GFTP', key='supported_checksums', value='md5')
+    add_rse('CNAF_StoRM', 'root')
+    add_protocol('CNAF_StoRM', params)
+    add_rse_attribute(rse='CNAF_StoRM', key='istape', value='False')
+    add_rse_attribute(rse='CNAF_StoRM', key='supported_checksums', value='adler32')
 
     # Now set a quota for root and jdoe on the 2 RSEs
     set_account_limit('root', get_rse_id('SITE1_DISK'), 100000000000)
