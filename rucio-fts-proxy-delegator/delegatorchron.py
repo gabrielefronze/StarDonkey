953912eb593abebf29bddfeb73d90d010200eeec
gabrielefronze <sucre.91@hotmail.it>
2019-06-10 16:27:00 +0200

    Delegator script

---
+++ rucio-fts-proxy-delegator/delegatorchron.py
@@ -0,0 +1,45 @@
+import os
+import time
+from datetime import datetime, timedelta
+import calendar
+import fts3.rest.client.easy as fts3
+
+month_to_number = {v: k for k,v in enumerate(calendar.month_abbr)}
+
+def voms_proxy_init(args = ''):
+    d = dict()
+
+    cmd = 'voms-proxy-init '+args
+    output = os.popen(cmd).read()
+    if len(output)>=4:
+        if len(output)==5:
+            output = output[1:]
+        output_lines = output.splitlines()[1::2]
+        proxy_path = output_lines[0].replace("Created proxy in ",'')[:-1]
+        proxy_expiration = output_lines[1].replace("Your proxy is valid until ",'')
+        proxy_expiration_datetime = datetime.strptime(proxy_expiration.partition(' ')[2].replace('UTC ',''), '%b %d %H:%M:%S %Y')
+        proxy_expiration_timestamp = proxy_expiration_datetime.strftime('%s')
+
+        d['path'] = proxy_path
+        d['expiration'] = proxy_expiration_datetime
+        d['TS'] = proxy_expiration_timestamp
+
+        return d
+    else:
+        print('FATAL: an error occurred. See below for details:')
+        print(output)
+        return d
+
+
+if __name__ == '__main__':
+    proxy = voms_proxy_init()
+    if proxy:
+        print(proxy['path'])
+        print(proxy['expiration'])
+        print(proxy['TS'])
+    else:
+        print("FATAL: proxy creating failed.")
+
+    os.environ["X509_USER_PROXY"] = proxy['path']
+    fts3_context = context = fts3.Context('https://fts3-public.cern.ch:8446', verify=True)
+    delegation_ID = fts3.delegate(fts3_context, lifetime=timedelta(minutes=2), force=False)
