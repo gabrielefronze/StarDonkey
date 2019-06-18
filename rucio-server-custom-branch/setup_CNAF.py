94c596d6bc5342fc98db0bb029551b047029da92
gabrielefronze <sucre.91@hotmail.it>
2019-06-18 12:58:24 +0200

    Importing add_distance from rucio.rse

--- rucio-server-custom-branch/setup_CNAF.py
+++ rucio-server-custom-branch/setup_CNAF.py
@@ -15,7 +15,7 @@ from rucio.api.account import add_account
 from rucio.api.identity import add_account_identity
 from rucio.api.scope import add_scope
 from rucio.api.did import add_did
-from rucio.api.rse import add_rse
+from rucio.api.rse import add_rse, add_distance
 from rucio.db.sqla.util import build_database, create_root_account
 from rucio.core.account_limit import set_account_limit
 from rucio.core.rse import add_protocol, get_rse_id, add_rse_attribute
