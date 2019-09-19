c54f41f537da1d2c910afe01591e34d3ae23c2a4
gabrielefronze <sucre.91@hotmail.it>
2019-09-19 12:33:25 +0200

    Adding self signed CA certs to fts server

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
 ./rucio-common-cfg/propagate-config.sh
+
+cp ./CA-stuff/rucio-fts3.crt.pem ./rucio-fts3
+cp ./CA-stuff/rucio-fts3.key.pem ./rucio-fts3
+cp ./CA-stuff/stardonkey-CA.crt ./rucio-fts3
+
 docker-compose build
--- rucio-fts3/docker-entrypoint-slateci.sh
+++ rucio-fts3/docker-entrypoint-slateci.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
 
-mkdir -p /tmp/fts3-host-pems
-cd/tmp/fts3-host-pems
-
-openssl genrsa -des3 -out CA.key 4096
-openssl req -x509 -new -nodes -key CA.key -sha256 -days 1024 -out CA.crt
-openssl req -x509 -key CA.crt -keyout hostkey.pem -out hostcert.pem -days 365
 echo ## put on your nginx virtualhost ##
 echo ssl on;
 echo ssl_certificate      ./hostcert.pem;
@@ -14,12 +8,16 @@ echo ssl_certificate_key  ./hostkey.pem;
 cp /tmp/fts3-host-pems/hostcert.pem /etc/grid-security/
 chmod 644 /etc/grid-security/hostcert.pem
 chown root:root /etc/grid-security/hostcert.pem
+
 cp /tmp/fts3-host-pems/hostkey.pem /etc/grid-security/
 chmod 400 /etc/grid-security/hostkey.pem
 chown root:root /etc/grid-security/hostkey.pem
+
 cp /tmp/fts3-configs/ca.crt /etc/grid-security/certificates/docker-entrypoint_ca.crt
+
 cp /tmp/fts3-configs/fts3config /etc/fts3/fts3config
 cp /tmp/fts3-configs/fts-msg-monitoring.conf /etc/fts3/fts-msg-monitoring.conf
+
 mkdir -p /var/lib/mysql/
 mkdir -p /var/log/fts3rest/ && touch /var/log/fts3rest/fts3rest.log
 chown fts3:fts3 /var/log/
